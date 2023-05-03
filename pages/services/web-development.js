0d96b074bc784fd4dcafb1d116b217330d59ff1f
Horizon-dev4 <horizonsolutions.dev4@gmail.com>
2023-05-03 15:09:02 +0600

    icons added in web dev service

--- pages/services/web-development.js
+++ pages/services/web-development.js
@@ -24,9 +24,9 @@ const WebDevelopment = () => {
               </h2>
             </div>
 
-            <div className="row" style={{'marginTop' : '80px'}}>
-              <div className="col-md-6">
-                <p className="text-start" style={{ paddingTop: "90px" }}>
+            <div className="row" >
+              <div className="col-md-6 srvc_hero_align">
+                <p className="text-start">
                   For creating appealing and user-friendly websites, Kodeend IT
                   Solutions provides the best full-stack web development
                   services. Our all-encompassing strategy guarantees a smooth
@@ -36,7 +36,7 @@ const WebDevelopment = () => {
               </div>
               <div className="col-md-6">
                 <Image
-                  src="/assets/images/web-development.jpg"
+                  src="/assets/images/service1_1.jpg"
                   className="img-fluid"
                   width={700}
                   height={600}
@@ -56,9 +56,9 @@ const WebDevelopment = () => {
               <div class="col">
                 <div class="card h-100">
                   <Image
-                    src="/assets/images/web-development.jpg"
+                    src="/assets/images/service1_2.jpg"
                     class="card-img-top"
-                    width={400}
+                    width={200}
                     height={220}
                     alt="UI/UX Design"
                   />
@@ -75,17 +75,17 @@ const WebDevelopment = () => {
               <div class="col">
                 <div class="card h-100">
                   <Image
-                    src="/assets/images/web-development.jpg"
+                    src="/assets/images/service1_3.jpg"
                     class="card-img-top"
-                    width={400}
+                    width={200}
                     height={220}
                     alt="Front-End Development"
                   />
                   <div class="card-body">
                     <h5 class="card-title">Front-End Development</h5>
                     <p class="card-text">
-                      HTML, CSS, JavaScript, and frameworks such as React and
-                      Vue are used in responsive web development.
+                      HTML, CSS, JavaScript, and frameworks such as React js and
+                      Next js are used in responsive Front-End development.
                     </p>
                   </div>
                 </div>
@@ -94,9 +94,9 @@ const WebDevelopment = () => {
               <div class="col">
                 <div class="card h-100">
                   <Image
-                    src="/assets/images/web-development.jpg"
+                    src="/assets/images/service1_4.jpg"
                     class="card-img-top"
-                    width={400}
+                    width={200}
                     height={220}
                     alt="Back-End Development"
                   />
@@ -104,7 +104,7 @@ const WebDevelopment = () => {
                     <h5 class="card-title">Back-End Development</h5>
                     <p class="card-text">
                       Server-side programming that is robust, utilizing
-                      technologies such as Node.js, Python, and PHP.
+                      technologies such as Node.js, Python, Django, PHP and Laravel.
                     </p>
                   </div>
                 </div>
@@ -112,22 +112,22 @@ const WebDevelopment = () => {
             </div>
           </div>
 
+          {/* Second row of features */}
           <div className="mt-4">
             <div class="row row-cols-1 row-cols-md-3 g-4">
               <div class="col">
                 <div class="card h-100">
                   <Image
-                    src="/assets/images/web-development.jpg"
+                    src="/assets/images/service1_5.jpg"
                     class="card-img-top"
-                    width={400}
+                    width={200}
                     height={220}
                     alt="UI/UX Design"
                   />
                   <div class="card-body">
-                    <h5 class="card-title">UI/UX Design</h5>
+                    <h5 class="card-title">Custom Website Design</h5>
                     <p class="card-text">
-                      Web design that is engaging and user-centric in order to
-                      provide the best user experience and functionality.
+                      Customized website solutions for your business requirements and branding.
                     </p>
                   </div>
                 </div>
@@ -135,17 +135,73 @@ const WebDevelopment = () => {
               <div class="col">
                 <div class="card h-100">
                   <Image
-                    src="/assets/images/web-development.jpg"
+                    src="/assets/images/service1_6.jpg"
+                    class="card-img-top"
+                    width={200}
+                    height={220}
+                    alt="Front-End Development"
+                  />
+                  <div class="card-body">
+                    <h5 class="card-title">Content Management System (CMS)</h5>
+                    <p class="card-text">
+                      CMSs that are simple to use, like WordPress and Wix, providing an ideal one for managing website content.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="col">
+                <div class="card h-100">
+                  <Image
+                    src="/assets/images/service1_7.jpg"
+                    class="card-img-top"
+                    width={200}
+                    height={220}
+                    alt="Back-End Development"
+                  />
+                  <div class="card-body">
+                    <h5 class="card-title">E-commerce Integration</h5>
+                    <p class="card-text">
+                      E-commerce systems to make it easy for businesses to sell your products and services online.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Third row of features */}
+          <div className="mt-4">
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+              <div class="col">
+                <div class="card h-100">
+                  <Image
+                    src="/assets/images/service1_8.jpg"
+                    class="card-img-top"
+                    width={200}
+                    height={220}
+                    alt="UI/UX Design"
+                  />
+                  <div class="card-body">
+                    <h5 class="card-title">Content & SEO</h5>
+                    <p class="card-text">
+                      SEO optimization and high-quality content production for website visibility and search engine ranking.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="col">
+                <div class="card h-100">
+                  <Image
+                    src="/assets/images/service1_9.jpg"
                     class="card-img-top"
                     width={400}
                     height={220}
                     alt="Front-End Development"
                   />
                   <div class="card-body">
-                    <h5 class="card-title">Front-End Development</h5>
+                    <h5 class="card-title">Graphics</h5>
                     <p class="card-text">
-                      HTML, CSS, JavaScript, and frameworks such as React and
-                      Vue are used in responsive web development.
+                      Graphics and images that are customized to improve website design and user engagement.
                     </p>
                   </div>
                 </div>
@@ -153,17 +209,16 @@ const WebDevelopment = () => {
               <div class="col">
                 <div class="card h-100">
                   <Image
-                    src="/assets/images/web-development.jpg"
+                    src="/assets/images/service1_10.jpg"
                     class="card-img-top"
-                    width={400}
+                    width={200}
                     height={220}
                     alt="Back-End Development"
                   />
                   <div class="card-body">
-                    <h5 class="card-title">Back-End Development</h5>
+                    <h5 class="card-title">Website Maintenance, Support & Security</h5>
                     <p class="card-text">
-                      Server-side programming that is robust, utilizing
-                      technologies such as Node.js, Python, and PHP.
+                      Regular website maintenance, technical support, and security upgrades to ensure peak website performance and security.
                     </p>
                   </div>
                 </div>
@@ -174,7 +229,7 @@ const WebDevelopment = () => {
 
           {/* get a quote section starts */}
           <div className="layout_padding-top">
-            <div className="service_heading" style={{'paddingBottom':'0px!important'}}>
+            <div className="request_service_heading">
               <h2>Request a Quote</h2>
             </div>
           </div>
