2f11fadc531a7af5a0dfb238e2da900766745cb9
Horizon-dev4 <horizonsolutions.dev4@gmail.com>
2023-05-03 16:00:19 +0600

    request a quote form is activated

--- components/QuoteForm.js
+++ components/QuoteForm.js
@@ -7,27 +7,28 @@ const QuoteForm = () => {
             <div className="row">
                   <div className="col-md-9 mx-auto">
                     <div className="contact-form">
-                      <form>
+                      <form action="https://formspree.io/f/mvonlryr" method="POST">
                         <div className="row">
-                        <div className='col-md-6'>
-                          <input type="text" placeholder="Your Name " name='UserName' />
-                        </div>
-                        <div className='col-md-6'>
-                          <input type="email" placeholder="Email Address" name='Email' />
-                        </div>
-                        <div className='col-md-12'>
-                          <input
-                            type="text"
-                            name='Message'
-                            placeholder="Message"
-                            className="input_message"
-                          />
-                        </div>
-                        <div className="d-flex justify-content-center">
-                          <button type="submit" className="btn_on-hover">
-                            Send
-                          </button>
-                        </div>
+                          <div className='col-md-6'>
+                            <input type="text" placeholder="Your Name " name='UserName' required />
+                          </div>
+                          <div className='col-md-6'>
+                            <input type="email" placeholder="Email Address" name='Email' required />
+                          </div>
+                          <div className='col-md-12'>
+                            <input
+                              type="text"
+                              name='Message'
+                              placeholder="Message"
+                              className="input_message"
+                              required
+                            />
+                          </div>
+                          <div className="d-flex justify-content-center">
+                            <button type="submit" className="btn_on-hover">
+                              Send
+                            </button>
+                          </div>
                         </div>
                       </form>
                     </div>
